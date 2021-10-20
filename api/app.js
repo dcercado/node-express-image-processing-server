2eb33d9a58705edfc98828f70e4426ae84eeee71
Daniel Cercado <nt_dcercado@mecojax.com>
2021-10-20 17:21:38 -0400

    Completed module

---
+++ api/app.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+const app = express();
+const pathToIndex = index.html;
+path.resolve(__dirname, /client/index.html) = pathToIndex;
+
+module.exports= {app};
