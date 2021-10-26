b3ce1fc23729113f74974b57f86de36c5d4819df
dcercado <87337079+dcercado@users.noreply.github.com>
2021-10-26 19:04:27 -0400

    Completed Module
    this is were I finish part 2 of the module.

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var path = require('path');
 const app = express();
-const pathToIndex = index.html;
-path.resolve(__dirname, /client/index.html) = pathToIndex;
+const pathToIndex;
+path.resolve(__dirname, '../client/index.html') = pathToIndex;
+app.use('/*', function(req, res){
+res.sendFile(pathToIndex)
 
-module.exports= {app};
+})
+module.exports= app;
