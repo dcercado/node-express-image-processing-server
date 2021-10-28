c5e9d88895e126f26cb67cd57988f4ff4590c3cb
dcercado <87337079+dcercado@users.noreply.github.com>
2021-10-28 16:21:18 -0400

    module 1 work

--- api/app.js
+++ api/app.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var path = require('path');
-const app = express();
-const pathToIndex;
-path.resolve(__dirname, '../client/index.html') = pathToIndex;
-app.use('/*', function(req, res){
-res.sendFile(pathToIndex)
 
-})
-module.exports= app;
+const express = require ('express');
+const path = require ('path');
+const app = express();
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
+app.use('/*',()=>
+{'help'})
+module.exports = app;
+
